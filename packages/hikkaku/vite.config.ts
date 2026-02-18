83262a553bd6712077d8f2c89d9185a97d4a2087
Shotaro Nakamura <syoutarou1103@outlook.jp>
2026-02-18 18:13:31 +0900

    feat: add codecov

--- packages/hikkaku/vite.config.ts
+++ packages/hikkaku/vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite-plus'
 export default defineConfig({
   test: {
     include: ['./src/**/*.test.ts'],
+    coverage: {
+      reporter: ['lcov'],
+    },
   },
 })
--- packages/testing/vite.config.ts
+++ packages/testing/vite.config.ts
@@ -7,5 +7,8 @@ export default defineConfig({
   },
   test: {
     include: ['./src/**/*.test.ts'],
+    coverage: {
+      reporter: ['lcov'],
+    },
   },
 })
